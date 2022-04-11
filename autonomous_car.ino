8b5bcb906ccdb29a10b2ff55c86108804e427df6
Autocar <Autocar@Autocar>
2022-04-11 16:08:25 -0700

    We finished the bravo task, added the flag to indicate the end.
    Signed-off-by: Autocar <Autocar@Autocar>

--- autonomous_car.ino
+++ autonomous_car.ino
@@ -3,6 +3,7 @@
 Servo left_motor;
 Servo right_motor;
 Servo myservo;
+Servo flag_servo;
 
 
 int left_speed = 0;
@@ -15,15 +16,36 @@ void setup() {
   left_motor.attach(5);
   right_motor.attach(7);
   myservo.attach(2);
+  flag_servo.attach(8);
   
 }
 
 void loop() {
-
   center();
-  left();
-  forward(25, 20);     // Go forward at 10% for 2 seconds
   delay(500);
+  forward(30, 12);
+  delay(500);
+  left();
+  forward(30, 14);     // Go forward at 10% for 2 seconds
+  delay(500);
+  //forward(30, 6);
+  //reverse(30, 6);
+  center();
+  delay(500);
+  forward(30, 10);
+  delay(500);
+  right();
+  forward(30, 2.5);
+  center();
+  flag_up();
+  // delay(250);   FROMT THIS AND UP
+ // delay(500);
+  //right();
+  //delay(500);
+  //forward(30, 6);
+  //forward(30, 3);
+  //delay(500);
+
 
   //left();       // Go right at 10% for 2 seconds
   //delay(500);
@@ -61,10 +83,13 @@ void reverse(int speed_percent, int wait_time){
   zero();
 }
 
-
+void flag_up(){
+  flag_servo.writeMicroseconds(220); //Dont do to strong, it could break stuff.
+  delay(3000);
+}
 
 void right(){
-  myservo.writeMicroseconds(1900); //Dont do to strong, it could break stuff.
+  myservo.writeMicroseconds(2300); //Dont do to strong, it could break stuff.
   delay(3000);
 }
 
@@ -72,8 +97,9 @@ void center(){
   myservo.writeMicroseconds(1500); //Dont do to strong, it could break stuff.
   delay(3000);
 }
+
 ///rn delaty is 3 seconds so it will turn then wait 3 seconds
 void left(){
-  myservo.writeMicroseconds(1100); //Dont do to strong, it could break stuff.
+  myservo.writeMicroseconds(700); //Dont do to strong, it could break stuff.
   delay(3000); 
 }
